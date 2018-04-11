8a8c11806215dc825745165cee5f8062b6a75451
Kirmark <37423353+Kirmark@users.noreply.github.com>
2018-04-11 22:41:29 +0300

    Update mumble_notice.py

--- mumble_notice.py
+++ mumble_notice.py
@@ -7,7 +7,7 @@ from systemd import journal
 def god_notice(line):
     curr_mess = ''
     match_in = re.search(r'=> <([0-9]*):(?P<user>[^ ]*)\(-([0-9]*)\)> Authenticated', line)
-    match_out = re.search(r'=> <([0-9]*):(?P<user>[^ ]*)\(-([0-9]*)\)> Connection closed', line)
+    #match_out = re.search(r'=> <([0-9]*):(?P<user>[^ ]*)\(-([0-9]*)\)> Connection closed', line)
     if match_in:
         curr_mess = match_in.group('user') + ' in'
     #elif match_out:
