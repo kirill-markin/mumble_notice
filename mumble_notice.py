f41107d55bd0e1e6a90c92c932f0d6b092d5da28
Kirmark <37423353+Kirmark@users.noreply.github.com>
2018-04-12 03:08:12 +0300

    Update mumble_notice.py

--- mumble_notice.py
+++ mumble_notice.py
@@ -7,9 +7,8 @@ import requests
 import json
 
 import logging
-from getpass import getpass
-from argparse import ArgumentParser
-import slixmpp
+from sleekxmpp import ClientXMPP
+from sleekxmpp.exceptions import IqError, IqTimeout
 
 with open('conf.json', 'r') as f:
     config = json.load(f)
@@ -26,6 +25,67 @@ def god_notice(line):
     #elif match_out:
     #    curr_mess = match_out.group('user') + ' out of mumble'
     return curr_mess
+
+################
+
+
+class EchoBot(ClientXMPP):
+
+    def __init__(self, jid, password):
+        ClientXMPP.__init__(self, jid, password)
+
+        self.add_event_handler("session_start", self.session_start)
+        self.add_event_handler("message", self.message)
+
+        # If you wanted more functionality, here's how to register plugins:
+        # self.register_plugin('xep_0030') # Service Discovery
+        # self.register_plugin('xep_0199') # XMPP Ping
+
+        # Here's how to access plugins once you've registered them:
+        # self['xep_0030'].add_feature('echo_demo')
+
+        # If you are working with an OpenFire server, you will
+        # need to use a different SSL version:
+        # import ssl
+        # self.ssl_version = ssl.PROTOCOL_SSLv3
+
+    def session_start(self, event):
+        self.send_presence()
+        self.get_roster()
+
+        # Most get_*/set_* methods from plugins use Iq stanzas, which
+        # can generate IqError and IqTimeout exceptions
+        #
+        # try:
+        #     self.get_roster()
+        # except IqError as err:
+        #     logging.error('There was an error getting the roster')
+        #     logging.error(err.iq['error']['condition'])
+        #     self.disconnect()
+        # except IqTimeout:
+        #     logging.error('Server is taking too long to respond')
+        #     self.disconnect()
+
+    def message(self, msg):
+        if msg['type'] in ('chat', 'normal'):
+            msg.reply("Thanks for sending\n%(body)s" % msg).send()
+
+
+if __name__ == '__main__':
+    # Ideally use optparse or argparse to get JID,
+    # password, and log level.
+
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(levelname)-8s %(message)s')
+
+    xmpp = EchoBot('somejid@example.com', 'use_getpass')
+    xmpp.connect()
+    xmpp.process(block=True)
+
+
+
+
+#############
 
 j = journal.Reader()
 j.log_level(journal.LOG_INFO)
